a1bf3d66998c0b820ed8540a4e5799fa2922edb6
katelynsun <89225907+katelynsun@users.noreply.github.com>
2021-12-19 21:00:21 -0800

    phpupdate

--- index.php
+++ index.php
@@ -3,6 +3,30 @@
 
 	if (isset($_POST['liked'])) {
 		$postid = $_POST['postid'];
-		$result = mysqli_query($con, "SELECT * FROM posts WHERE id=$postid");
+		$result = mysqli_query($con, "SELECT * FROM posts 
+WHERE id=$postid");
 		$row = mysqli_fetch_array($result);
 		$n = $row['likes'];
+
+		mysqli_query($con, "INSERT INTO likes (userid, postid)
+VALUES (1, $postid)");
+
+		mysqli_query($con, "UPDATE posts SET likes=$n+1 WHERE
+id=$postid");
+
+		echo $n+1;
+		exit ();
+	}
+	if (isset($_POST['unliked'])) {
+		$postid = $_POST['postid'];
+		$result = mysqli_query($con, "SELECT * FROM posts 
+WHERE id=$postid");
+		$row = mysqli_fetch_array($result);
+		$n = $row['likes'];
+
+		mysqli_query($con, "UPDATE posts SET likes=$n-1 WHERE
+id=$posted");
+
+		echo $n-1;
+		exit();
+	}
